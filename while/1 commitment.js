da781b26a5607c0d6baac18539ef7e7a71b0ba33
Jahed Hossen <154004144+Jahed-Hossen@users.noreply.github.com>
2025-10-20 21:29:55 +0600

    Update 1 commitment.js

--- while/1 commitment.js
+++ while/1 commitment.js
@@ -1,7 +1,11 @@
+for (let i = 0; i < 6; i++) {
+  console.log("I will invest at least 6 hrs every single day for next 60 days!");
+}
 /***
 
 "I will invest at least 6 hrs every single day for next 60 days!" this message 60 times. So display this.
 
  */
+
 
 /*programming hero*/
